227a57bd9354d0d8fa5fdbcb8ff0fa4ff598fd8e
Jacek Trębicki <jacek.trebicki@mediaflex.pl>
2022-07-07 10:45:15 +0200

    fix(): Removed unused files

--- config/bundles.php
+++ config/bundles.php
@@ -5,5 +5,4 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
 ];
